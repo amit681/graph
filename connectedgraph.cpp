f2f22539a623733b1ef46fe4e42311788d2bf0d0
amit681 <63961834+amit681@users.noreply.github.com>
2020-05-05 01:26:38 -0700

    Add files via upload

---
+++ connectedgraph.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int e,n,k,i;
+cin >> n>>e>>k;
+for(i=0;i<e;i++)
+{
+int a,b;
+cin>>a>>b;
+}
+if(n-e>k)
+cout<<-1;
+else
+cout<<e-n+k;
+}
+
+https://www.hackerearth.com/practice/algorithms/graphs/graph-representation/practice-problems/algorithm/so-np-c559f406/
