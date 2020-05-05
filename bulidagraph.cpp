4626343aad6bd62e1423d4888a9b6d43d1dfc67e
amit681 <63961834+amit681@users.noreply.github.com>
2020-05-05 02:31:23 -0700

    Add files via upload

---
+++ bulidagraph.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>=12)
+    cout<<"Yes\n";
+   else cout<<"No\n";
+}
+
+
+
+https://www.hackerearth.com/practice/algorithms/graphs/graph-representation/practice-problems/algorithm/build-a-graph-5f5c6b4a/
